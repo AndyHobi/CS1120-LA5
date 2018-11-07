e9afcf3048d699d1a893bd472eb3c5c282eaaa5a
lucas <lucas@10.80.148.227>
2018-11-07 11:51:41 -0500

    Finished the formating and printing of the phone number method

--- LA5/src/edu/wmich/cs1120/LA5/FormatExceptionHandler.java
+++ LA5/src/edu/wmich/cs1120/LA5/FormatExceptionHandler.java
@@ -12,8 +12,63 @@ public class FormatExceptionHandler implements IFormatExceptionHandler {
 
 	@Override
 	public void handlePhoneNumberFormatException(PhoneNumberFormatException e) {
-		string unformatedNumber = e.getNumber();
+		String ufNumber = e.getNumber();
+		int count = 0;
+		char[] charArray = new char[ufNumber.length()];
+		int[] intArray = new int[9];
 		
+		for(int i = 0; i < ufNumber.length(); i++) {
+			charArray[i] = new Character(ufNumber.charAt(i));
+		}
+		
+		for(int i = 0; i < charArray.length; i++) {
+			switch (charArray[i]) {
+				case 0:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 1:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 2:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 3:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 4:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 5:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 6:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 7:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 8:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+				case 9:
+					intArray[count] = charArray[i];
+					count ++;
+					break;
+			}
+		}
+		
+		System.out.println("(" + intArray[0] + intArray[1] + intArray[2] + ")" +
+						    intArray[3] + intArray[4] + intArray[5] + "-" + 
+				            intArray[6] + intArray[7] + intArray[8] + intArray[9]);
 		
 	}
 
