32e88c33bcbba9dd5727864f7e233c1fce13da05
Admin <Admin@10.80.144.43>
2018-11-07 11:54:06 -0500

    Andy - work on contact Info Formatter

--- LA5/src/edu/wmich/cs1120/LA5/ContactInformationFormatter.java
+++ LA5/src/edu/wmich/cs1120/LA5/ContactInformationFormatter.java
@@ -1,16 +1,32 @@
 package edu.wmich.cs1120.LA5;
 
-public class ContactInformationFormatter implements IContactInformationFormatter{
+import java.io.File;
+import java.util.Scanner;
 
+public class ContactInformationFormatter implements IContactInformationFormatter{
+	
+	
 	@Override
 	public void readContactInformation(String[] filePaths) {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
 
 	@Override
 	public void formatContactInformation(String fileName) {
-		// TODO Auto-generated method stub
+		
+		File file = new File(fileName);
+		Scanner inputFile = new Scanner(fileName);
+		
+		String name = inputFile.nextLine();
+		String number = inputFile.nextLine();
+		String email = inputFile.nextLine();
+		
+		try { formatName(name); }
+		catch(NameFormatException e) {
+			IFormatExceptionHandler.handleNameFormatException(e);
+		}
 		
 	}
 
@@ -28,7 +44,12 @@ public class ContactInformationFormatter implements IContactInformationFormatter
 
 	@Override
 	public void formatName(String name) throws NameFormatException {
-		// TODO Auto-generated method stub
+		
+		String[] FLName = name.trim().split(" ");
+		
+		if(FLName[0].charAt(0) > 64 && FLName[0].charAt(0) < 93) {
+			
+		}
 		
 	}
 
